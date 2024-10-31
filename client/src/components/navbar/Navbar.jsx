013afdd0881b8beaa1a4af27ad6eec21fa3f107c
Supakun Thata <supakunt.thata@gmail.com>
2024-10-31 16:00:39 +0700

    fixed bug

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,49 +1,102 @@
-import React, { useContext, useRef, useState } from 'react'
-import './navbar.css'
-import cart_icon from '../assets/cart_icon.png'
-import nav_dropdown from '../assets/nav_dropdown.png'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../../context/ShopContext'
+import React, { useContext, useRef, useState } from "react";
+import "./navbar.css";
+import cart_icon from "../assets/cart_icon.png";
+import nav_dropdown from "../assets/nav_dropdown.png";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../../context/ShopContext";
 
 const Navbar = () => {
-
-  const [menu, setMenu] = useState("shop")
-  const { getTotalCartItems } = useContext(ShopContext)
-  const menuRef = useRef()
+  const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
 
   const drop_toggle = (e) => {
-    menuRef.current.classList.toggle('nav-menu-visible')
-    e.target.classList.toggle('open')
-  }
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
 
   return (
-    <div className='navbar flex justify-around py-[1rem] shadow-[0px_1px_3px_-2px_black]'>
-      <Link className='flex items-center' onClick={() => setMenu('shop')} to={'/'}>
+    <div className="navbar flex justify-around py-[1rem] shadow-[0px_1px_3px_-2px_black]">
+      <Link
+        className="flex items-center"
+        onClick={() => setMenu("shop")}
+        to={"/"}
+      >
         <div className="nav-logo flex items-center gap-[5px]">
-          <p className='text-[#171717] text-[25px] font-black'>GOZA</p>
-          <p className='text-[#171717] text-[25px] font-semibold'>STORE</p>
+          <p className="text-[#171717] text-[25px] font-black">GOZA</p>
+          <p className="text-[#171717] text-[25px] font-semibold">STORE</p>
         </div>
       </Link>
-      <img className='nav-dropdown' onClick={drop_toggle} src={nav_dropdown} alt="" />
-      <ul ref={menuRef} className='nav-menu flex items-center list-none gap-[50px] text-[#111111] text-[17px] font-semibold z-10 '>
-        <li onClick={() => { setMenu("shop") }}><Link to={'/'}>Home</Link> {menu === "shop" ? <hr style={{ width: "100%" }} /> : <hr />}</li>
-        <li onClick={() => { setMenu("men") }}><Link to={'/mens'}>Men</Link> {menu === "men" ? <hr style={{ width: "100%" }} /> : <hr />}</li>
-        <li onClick={() => { setMenu("women") }}><Link to={'/womens'}>Women</Link> {menu === "women" ? <hr style={{ width: "100%" }} /> : <hr />}</li>
-        <li onClick={() => { setMenu("kids") }}><Link to={'/kids'}>Kids</Link> {menu === "kids" ? <hr style={{ width: "100%" }} /> : <hr />}</li>
+      <img
+        className="nav-dropdown"
+        onClick={drop_toggle}
+        src={nav_dropdown}
+        alt=""
+      />
+      <ul
+        ref={menuRef}
+        className="nav-menu flex items-center list-none gap-[50px] text-[#111111] text-[17px] font-semibold z-10 "
+      >
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link to={"/"}>Home</Link>{" "}
+          {menu === "shop" ? <hr style={{ width: "100%" }} /> : <hr />}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          <Link to={"/mens"}>Men</Link>{" "}
+          {menu === "men" ? <hr style={{ width: "100%" }} /> : <hr />}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <Link to={"/womens"}>Women</Link>{" "}
+          {menu === "women" ? <hr style={{ width: "100%" }} /> : <hr />}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link to={"/kids"}>Kids</Link>{" "}
+          {menu === "kids" ? <hr style={{ width: "100%" }} /> : <hr />}
+        </li>
       </ul>
       <div className="nav-login-cart flex items-center gap-[45px] cursor-pointer">
-        {localStorage.getItem('auth-token') ?
-          <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/') }} className='w-[95px] bg-black h-[42px] outline-none rounded-[75px] text-[16px] text-white font-medium'>Logout</button>
-          :
-          <Link onClick={() => setMenu(null)} to={'/login'}>
-            <button className='active:bg-[#f3f3f3] w-[95px] bg-white h-[42px] outline-none border-[1px] border-black rounded-[75px] text-[16px] text-[#515151] font-medium'>Login</button>
+        {localStorage.getItem("auth-token") ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("auth-token");
+              window.location.replace("/");
+            }}
+            className="w-[95px] bg-black h-[42px] outline-none rounded-[75px] text-[16px] text-white font-medium"
+          >
+            Logout
+          </button>
+        ) : (
+          <Link onClick={() => setMenu(null)} to={"/login"}>
+            <button className="active:bg-[#f3f3f3] w-[95px] bg-white h-[42px] outline-none border-[1px] border-black rounded-[75px] text-[16px] text-[#515151] font-medium">
+              Login
+            </button>
           </Link>
-        }
-        <Link onClick={() => setMenu(null)} to={'/cart'}><img className='w-[28px]' src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count w-[18px] h-[18px] bg-red-500 flex justify-center items-center rounded-[11px] text-white text-[12px] mt-[-30px] ml-[-53px]">{getTotalCartItems()}</div>
+        )}
+        <Link onClick={() => setMenu(null)} to={"/cart"}>
+          <img className="min-w-[28px]" src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count w-[18px] h-[18px] bg-red-500 flex justify-center items-center rounded-[11px] text-white text-[12px] mt-[-30px] ml-[-53px]">
+          {getTotalCartItems()}
+        </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/components/relatedProducts/RelatedProducts.jsx
+++ client/src/components/relatedProducts/RelatedProducts.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import data_product from '../assets/data'
-import Item from '../item/Item'
-import './relatedproducts.css'
+import React from "react";
+import data_product from "../assets/data";
+import Item from "../item/Item";
+import "./relatedproducts.css";
 
 const RelatedProducts = () => {
   return (
-    <div className='relatedproducts flex flex-col items-center gap-[10px] h-[90vh]'>
-      <h1 className='text-[50px] text-[#171717] font-medium'>Related Products</h1>
-      <hr className='w-[200px] h-[6px] rounded-[10px] bg-[#252525]' />
+    <div className="relatedproducts flex flex-col items-center gap-[10px] mb-[10rem]">
+      <h1 className="text-[50px] text-[#171717] font-medium">
+        Related Products
+      </h1>
+      <hr className="w-[200px] h-[6px] rounded-[10px] bg-[#252525]" />
       <div className="relatedproducts-item mt-[50px] flex gap-[30px]">
         {data_product.map((item, i) => {
-          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RelatedProducts
+export default RelatedProducts;
