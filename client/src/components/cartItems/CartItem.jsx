7765a23d51c06cd812a1127cf119ce984365ba7e
Supakun Thata <supakunt.thata@gmail.com>
2024-11-09 08:58:17 +0700

    fixed price bug

--- client/src/components/cartItems/CartItem.jsx
+++ client/src/components/cartItems/CartItem.jsx
@@ -1,18 +1,23 @@
-import React, { useContext, useEffect } from 'react'
-import { ShopContext } from '../../context/ShopContext'
-import remove_icon from '../assets/cart_cross_icon.png'
-import './cartitem.css'
+import React, { useContext, useEffect } from "react";
+import { ShopContext } from "../../context/ShopContext";
+import remove_icon from "../assets/cart_cross_icon.png";
+import "./cartitem.css";
 
 const CartItem = () => {
-
-  const { all_product, cartTotal, cartItems, removeFromCart, getTotalCartAmount } = useContext(ShopContext)
+  const {
+    all_product,
+    cartTotal,
+    cartItems,
+    removeFromCart,
+    getTotalCartAmount,
+  } = useContext(ShopContext);
 
   useEffect(() => {
-    getTotalCartAmount()
-  }, [removeFromCart])
+    getTotalCartAmount();
+  }, [removeFromCart]);
 
   return (
-    <div className='cartitems m-[100px_170px]'>
+    <div className="cartitems m-[100px_170px]">
       <div className="cartitems-format-main grid items-center gap-[75px] py-[20px] text-[#454545] text-[16px] font-semibold grid-cols-[0.5fr_2fr_1fr_1fr_1fr_1fr]">
         <p>Products</p>
         <p>Title</p>
@@ -21,30 +26,46 @@ const CartItem = () => {
         <p>Total</p>
         <p>Remove</p>
       </div>
-      <hr className='h-[3px] bg-[#e2e2e2] border-0' />
+      <hr className="h-[3px] bg-[#e2e2e2] border-0" />
       {all_product.map((e) => {
-        if (cartItems[e.id] > 0) { // เช็ค AllproductID ว่า value ตัวไหนมีค่ามากกว่า 1 
-          return <div >
-            <div className="cartitems-format grid items-center gap-[75px] py-[20px] text-[#454545] text-[15px] font-medium grid-cols-[0.5fr_2fr_1fr_1fr_1fr_1fr]">
-              <img className='carticon-product-icon object-cover' src={e.image} alt="" />
-              <p>{e.name}</p>
-              <p>{e.new_price}</p>
-              <div>
-                <button className='cartitems-quantity w-[64px] h-[50px] border-solid border-[2px] border-[#ebebeb] bg-[#FFF]'>{cartItems[e.id]}</button>
+        if (cartItems[e.id] > 0) {
+          // เช็ค AllproductID ว่า value ตัวไหนมีค่ามากกว่า 1
+          return (
+            <div>
+              <div className="cartitems-format grid items-center gap-[75px] py-[20px] text-[#454545] text-[15px] font-medium grid-cols-[0.5fr_2fr_1fr_1fr_1fr_1fr]">
+                <img
+                  className="carticon-product-icon object-cover"
+                  src={e.image}
+                  alt=""
+                />
+                <p>{e.name}</p>
+                <p>{e.new_price}</p>
+                <div>
+                  <button className="cartitems-quantity w-[64px] h-[50px] border-solid border-[2px] border-[#ebebeb] bg-[#FFF]">
+                    {cartItems[e.id]}
+                  </button>
+                </div>
+                <p>${(e.new_price * cartItems[e.id]).toFixed(2)}</p>
+                <div>
+                  <img
+                    className="cartitems-remove-icon w-[15px] mx-[25px] cursor-pointer"
+                    src={remove_icon}
+                    onClick={() => {
+                      removeFromCart(e.id);
+                    }}
+                    alt=""
+                  />
+                </div>
               </div>
-              <p>${e.new_price * cartItems[e.id]}</p>
-              <div>
-                <img className='cartitems-remove-icon w-[15px] mx-[25px] cursor-pointer' src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" />
-              </div>
+              <hr className="h-[3px] bg-[#e2e2e2] border-0" />
             </div>
-            <hr className='h-[3px] bg-[#e2e2e2] border-0' />
-          </div>
+          );
         }
         return null;
       })}
       <div className="cartitems-down flex my-[70px]">
         <div className="cartitems-total flex-1 flex flex-col mr-[200px] gap-[20px]">
-          <h1 className='text-[24px] font-semibold'>Order Summary</h1>
+          <h1 className="text-[24px] font-semibold">Order Summary</h1>
           <div>
             <div className="cartitems-total-item flex justify-between py-[15px]">
               <p>Subtatal</p>
@@ -57,22 +78,30 @@ const CartItem = () => {
             </div>
             <hr />
             <div className="cartitems-total-item flex justify-between py-[15px]">
-              <h3 className='text-[19px] font-semibold'>Total</h3>
-              <h3 className='text-[19px] font-semibold'>${cartTotal}</h3>
+              <h3 className="text-[19px] font-semibold">Total</h3>
+              <h3 className="text-[19px] font-semibold">${cartTotal}</h3>
             </div>
           </div>
-          <button className='w-[262px] h-[58px] outline-none border-none bg-black text-[#fff] text-[14px] font-medium cursor-pointer hover:text-[#adadad]'>PROCEED TO CHECKOUT</button>
+          <button className="w-[262px] h-[58px] outline-none border-none bg-black text-[#fff] text-[14px] font-medium cursor-pointer hover:text-[#adadad]">
+            PROCEED TO CHECKOUT
+          </button>
         </div>
         <div className="cartitems-promocode flex-1 text-[15px] font-medium">
-          <p className='text-[#555]'>If you a promo code, Enter it here</p>
+          <p className="text-[#555]">If you a promo code, Enter it here</p>
           <div className="cartitems-promobox flex items-center w-[504px] mt-[15px] pl-[20px] h-[58px] bg-[#eaeaea]">
-            <input className='border-none outline-none bg-transparent text-[14px] w-[330px] h-[50px]' type="text" placeholder='promo code' />
-            <button className='w-[170px] h-[58px] text-[14px] bg-black text-white cursor-pointer hover:text-[#adadad]'>Submit</button>
+            <input
+              className="border-none outline-none bg-transparent text-[14px] w-[330px] h-[50px]"
+              type="text"
+              placeholder="promo code"
+            />
+            <button className="w-[170px] h-[58px] text-[14px] bg-black text-white cursor-pointer hover:text-[#adadad]">
+              Submit
+            </button>
           </div>
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,24 +1,23 @@
-import React, { createContext, useEffect, useState } from 'react'
-import all_product from '../components/assets/all_product' // ** All product **
-
+import React, { createContext, useEffect, useState } from "react";
+import all_product from "../components/assets/all_product"; // ** All product **
 
 // create useContext
-export const ShopContext = createContext(null) // ชื่อ Stones ที่จะเรียกใช้
+export const ShopContext = createContext(null); // ชื่อ Stones ที่จะเรียกใช้
 
 // func get id สินค้าทุกอัน --> setProduct
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < 300 + 1; index++) { // จำนวนสินค้าทั้งหมด
+  for (let index = 0; index < 300 + 1; index++) {
+    // จำนวนสินค้าทั้งหมด
     cart[index] = 0; // { Allproduct_ID (key) : 0 (value) }
   }
-  return cart
-}
+  return cart;
+};
 
 const ShopContextProvider = (props) => {
-
-  const [cartItems, setCartItems] = useState(getDefaultCart())
-  const [cartTotal, setCartTotal] = useState(0)
-  const URL = import.meta.env.VITE_APP_API
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const [cartTotal, setCartTotal] = useState(0);
+  const URL = import.meta.env.VITE_APP_API;
 
   // const [all_product, setAll_product] = useState([])
 
@@ -27,83 +26,99 @@ const ShopContextProvider = (props) => {
     //   .then(res => res.json())
     //   .then(data => setAll_product(data))
 
-    if (localStorage.getItem('auth-token')) {
+    if (localStorage.getItem("auth-token")) {
       fetch(`${URL}/getcart`, {
         method: "POST",
         headers: {
-          Accept: 'application/from-data',
-          'auth-token': `${localStorage.getItem('auth-token')}`, //ส่ง token ไปเพื่อตรวจสอบ
-          'Content-Type': 'application/json'
+          Accept: "application/from-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`, //ส่ง token ไปเพื่อตรวจสอบ
+          "Content-Type": "application/json",
         },
-        body: ""
-      }).then(req => req.json()).then(data => setCartItems(data))
+        body: "",
+      })
+        .then((req) => req.json())
+        .then((data) => setCartItems(data));
     }
-  }, [])
+  }, []);
 
   // Function เพิ่มลบของลงในตะกร้า
   const addToCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     // {cart ( Object จาก getDefaultCart ), key( ID ตัวที่จะเปลี่ยน ) : value( ค่าที่จะรับมาใหม่ +1 ) }
     // console.log(cartItems)
 
     // ** Check login ** --> have token
-    if (localStorage.getItem('auth-token')) {
+    if (localStorage.getItem("auth-token")) {
       fetch(`${URL}/addtocart`, {
         method: "POST",
         headers: {
-          Accept: 'application/from-data',
-          'auth-token': `${localStorage.getItem('auth-token')}`, //ส่ง token ไปเพื่อตรวจสอบ
-          'Content-Type': 'application/json'
+          Accept: "application/from-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`, //ส่ง token ไปเพื่อตรวจสอบ
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ "itemId": itemId }) // ส่ง id ไปเพื่อตรวจสอบ
-      }).then(req => req.json()).then(data => console.log(data))
+        body: JSON.stringify({ itemId: itemId }), // ส่ง id ไปเพื่อตรวจสอบ
+      })
+        .then((req) => req.json())
+        .then((data) => console.log(data));
     }
-  }
+  };
   const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     // {cart ( Object จาก getDefaultCart ), key( ID ตัวที่จะเปลี่ยน ) : value( ค่าที่จะรับมาใหม่ -1 ) }
 
     // ** Check login ** --> have token
-    if (localStorage.getItem('auth-token')) {
+    if (localStorage.getItem("auth-token")) {
       fetch(`${URL}/removefromcart`, {
         method: "POST",
         headers: {
-          Accept: 'application/from-data',
-          'auth-token': `${localStorage.getItem('auth-token')}`, //ส่ง token ไปเพื่อตรวจสอบ
-          'Content-Type': 'application/json'
+          Accept: "application/from-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`, //ส่ง token ไปเพื่อตรวจสอบ
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ "itemId": itemId })
-      })
+        body: JSON.stringify({ itemId: itemId }),
+      });
     }
-  }
-
+  };
 
   // Function รวมราคาสินค้าทั้งหมด
   const getTotalCartAmount = () => {
-    let totalAmount = 0
-    for (const item in cartItems) { // loop สิ้นค้าทั้งหมดที่อยู่ในตะกร้าแล้วเอาไปเทียบกับสิ้นค้าทั้งหมดเพื่อหาผมรวมจำนวนราคา
-      if (cartItems[item] > 0) { // ถ้าในตะกร้ามีสินค้ามากกว่า 1 --> setuseState
-        let itemInfo = all_product.find((product) => product.id === Number(item))
-        totalAmount += itemInfo.new_price * cartItems[item]
-        setCartTotal(totalAmount)
-      } if (totalAmount === 0) { // ถ้าในตะกร้ามีสินค้า = 0 --> setuseState(0)
-        setCartTotal(0)
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      // loop สิ้นค้าทั้งหมดที่อยู่ในตะกร้าแล้วเอาไปเทียบกับสิ้นค้าทั้งหมดเพื่อหาผมรวมจำนวนราคา
+      if (cartItems[item] > 0) {
+        // ถ้าในตะกร้ามีสินค้ามากกว่า 1 --> setuseState
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+        setCartTotal(totalAmount.toFixed(2));
+      }
+      if (totalAmount === 0) {
+        // ถ้าในตะกร้ามีสินค้า = 0 --> setuseState(0)
+        setCartTotal(0);
       }
       // return totalAmount
     }
-  }
+  };
 
   // Function จำนวนสินค้าในตระกร้า
   const getTotalCartItems = () => {
-    let totalItem = 0
+    let totalItem = 0;
     for (const item in cartItems) {
-      totalItem += cartItems[item]
+      totalItem += cartItems[item];
     }
-    return totalItem
-  }
+    return totalItem;
+  };
   // Stones Context
-  const contextValue = { getTotalCartItems, cartTotal, all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount } // all_product data 
-
+  const contextValue = {
+    getTotalCartItems,
+    cartTotal,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+  }; // all_product data
 
   // console.log(addToCart(1))
 
@@ -111,7 +126,7 @@ const ShopContextProvider = (props) => {
     <ShopContext.Provider value={contextValue}>
       {props.children}
     </ShopContext.Provider>
-  )
-}
+  );
+};
 
-export default ShopContextProvider
+export default ShopContextProvider;
